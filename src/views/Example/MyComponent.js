85e44c15e57f5a78a9a354a434db4768fe52320f
dvh3152002 <dvh31502@gmail.com>
2022-12-10 22:49:43 +0700

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Hiep',
+        class: 'K65CNTTB'
+    }
+
+    /*
+        JSX => return 1 block
+    */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>My Component</div>
+            // <React.Fragment></React.Fragment>=> gõ đầy đủ
+            // gõ tắt
+            <>
+                <div className="first">
+                    My Component
+                </div>
+                <div><input value={this.state.name} type='text' onChange={(event) => this.handleOnChangeName(event)}></input></div>
+                <div className="second"> My name is {this.state.name}, My class is {this.state.class}</div>
+            </>
         )
     }
 }

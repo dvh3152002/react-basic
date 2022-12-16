31650a0a4da6ed5dd31dce47f056a974b6853fa4
dvh3152002 <dvh31502@gmail.com>
2022-12-16 12:38:49 +0700

    React Routers - Điều Hướng Trang Với React

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.scss';
-// import MyComponent from './Example/MyComponent';
+import MyComponent from './Example/MyComponent';
 import ListTodo from './Todos/ListTodo';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Nav from './Nav/Nav';
+import Home from './Example/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 /*
 2 components: class component / function component (arrow function / function thường)
@@ -12,33 +20,34 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   //App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
 
-        <ListTodo />
+          <Routes >
 
-        {/* <MyComponent /> */}
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/todos" element={<ListTodo />} />
+            <Route exact path="/about" element={<MyComponent />} />
+          </Routes >
+        </header>
 
-        {/* <MyComponent></MyComponent>      */}
-      </header>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </div>
+    </Router >
   );
 }
 

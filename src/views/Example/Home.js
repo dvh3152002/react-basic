e170f2c584afa62ecd9b71ff5446a28e2f59c0f8
dvh3152002 <dvh31502@gmail.com>
2022-12-17 17:26:38 +0700

    Importing Image - Sử Dụng Hình Ảnh Trong Ứng Dụng React

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,22 +1,28 @@
 import React from "react";
 import withRouter from "../HOC/withRouter";
 import Color from "../HOC/Color";
+import logo from "../../assets/images/logo.png"
 
 class Home extends React.Component {
     componentDidMount() {
-        setTimeout(() => {
-            console.log('check timeout')
-            // this.props.navigate('/todos')(Hook)
-            // this.props.history.push('/todos')(v5)
-        }, 3000)
+        // setTimeout(() => {
+        //     console.log('check timeout')
+        //     // this.props.navigate('/todos')(Hook)
+        //     // this.props.history.push('/todos')(v5)
+        // }, 3000)
     }
 
     render() {
         console.log("check props ", this.props)
         return (
-            <div>
-                Hello world
-            </div>
+            <>
+                <div>
+                    Hello world
+                </div>
+                <div>
+                    <img src={logo} />
+                </div>
+            </>
         )
     }
 }
